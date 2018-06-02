f35f8c05ee405d72c763da26dfd390070710a76b
Danilo Scavassa <dan.scavassa@gmail.com>
2018-06-02 19:06:08 -0300

    Added some layout and shared model

---
+++ src/app/shared/ingredients.model.ts
@@ -0,0 +1,6 @@
+// Nesse contrutor as variaveis name e amount
+// são criadas automaticamente pelo construor
+// da mesma forma que no modulo recipe
+export class Ingredient {
+    constructor (public name: string, public amount: number) {}
+}
