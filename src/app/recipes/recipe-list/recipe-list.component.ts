a708c19a1e66815a31d38193b4563ff7178d814f
Danilo Scavassa <dan.scavassa@gmail.com>
2018-06-02 20:11:26 -0300

    Showing recipe detail dynamically

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recepies: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/skillet-chicken-roasted-potatoes-carrots-ck.jpg?itok=dzdpIO4s'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/skillet-chicken-roasted-potatoes-carrots-ck.jpg?itok=dzdpIO4s')
+    new Recipe('A Second Test Recipe', 'This is simply a second test', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/skillet-chicken-roasted-potatoes-carrots-ck.jpg?itok=dzdpIO4s')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected (recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
