06253f466adaa45a5a79e1fccc1f399d7418b15c
Danilo Scavassa <dan.scavassa@gmail.com>
2018-06-02 19:24:02 -0300

    Adde some fake navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component } from "@angular/core";
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+    @Output() onSelectedPage = new EventEmitter<string>();
 
+    selectPage (pageName) {
+        this.onSelectedPage.emit(pageName);
+    }
 }
