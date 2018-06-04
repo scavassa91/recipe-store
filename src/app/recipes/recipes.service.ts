8a2313628401b5275a7a1f0f9647727ca23768e4
Danilo Scavassa <dan.scavassa@gmail.com>
2018-06-04 00:21:02 -0300

    Added 'add ingredients into shopping list' feature

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,16 +1,41 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredients.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipesService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recepies: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/skillet-chicken-roasted-potatoes-carrots-ck.jpg?itok=dzdpIO4s'),
-        new Recipe('A Second Test Recipe', 'This is simply a second test', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/skillet-chicken-roasted-potatoes-carrots-ck.jpg?itok=dzdpIO4s')
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super-tastu-schnitzel - just awesome!',
+            'https://www.recipetineats.com/wp-content/uploads/2017/08/Schnitzel-3-landscape.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+        ),
+        new Recipe(
+            'Big Fat Burger',
+            'What else you need to say?',
+            'https://i.pinimg.com/originals/d3/f3/48/d3f348e98ed526bcbcffe8bf4a7a6674.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 3)
+            ]
+        )
     ];
+    
+    constructor (private shoppingListService: ShoppingListService) {}
 
     getRecipes () {
         return this.recepies.slice();
+    }
+
+    addIngredientsToShoppingList (ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
