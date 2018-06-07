c3b2ec85663e54ef9d08a09fd019656aba774b7e
Danilo Scavassa <dan.scavassa@gmail.com>
2018-06-07 17:25:30 -0300

    Added routing with angular router

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
@@ -6,9 +6,4 @@ import { Component, Output, EventEmitter } from "@angular/core";
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-    @Output() onSelectedPage = new EventEmitter<string>();
-
-    selectPage (pageName) {
-        this.onSelectedPage.emit(pageName);
-    }
 }
